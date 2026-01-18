d27fc0fefd7cda16d16e190f76ab5956115acd6c
sayan <sd758286@gmail.com>
2026-01-18 20:25:54 +0530

    Fix CORS and backend URL

--- index.js
+++ index.js
@@ -9,10 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: [
-    "http://localhost:5173",        // local frontend
-    "https://sabuj-sangha.vercel.app" // production frontend
-  ],
+  origin: "*",
   methods: ["GET", "POST"]
 }));
 
